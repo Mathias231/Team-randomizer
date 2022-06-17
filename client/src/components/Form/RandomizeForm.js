e933c79ab2a6b365009cde9f450b83610373ccdd
Mathias231 <mathias.krogh@outlook.com>
2022-06-17 16:31:23 +0200

    First commit

---
+++ client/src/components/Form/RandomizeForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// Importing components
+import Input from '../Input/Input';
+
+export default function Randomize(props) {
+    return(
+        <div className="max-w-xl bg-black">
+            <Input />
+        </div>
+    )
+}
