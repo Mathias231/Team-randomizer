16a079230ec8dd934f571d51afcd9f004c1a2917
Mathias231 <mathias.krogh@outlook.com>
2022-06-17 16:30:55 +0200

    Importing RandomizeForm and fixed styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 // Importing components
-// import Input from './components/Input/Input';
+import Randomize from './components/Form/RandomizeForm';
+
 
 function App() {
   return (
     <div className="container mx-auto mt-5">
       <div className="grid grid-flow-col space-x-5">
-        <div className="flex col-span-4 border-2 rounded-md border-rose-500">
-          <h1></h1>
+        <div className="col-span-4 border-2 rounded-md border-rose-500">
+          <h1 className="text-2xl text-center font-bold">Generate Team</h1>
+          <Randomize />
         </div>
         <div className="col-span-2 border-2 rounded-md border-rose-500">
           <h1 className="text-2xl text-center font-bold">Information</h1>
