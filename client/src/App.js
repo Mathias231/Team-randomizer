8f82f4d6d09b39d19a123bafbc8ff263db1330c4
Mathias231 <mathias.krogh@outlook.com>
2022-06-17 15:26:07 +0200

    Adding title for each grid

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,11 @@ function App() {
   return (
     <div className="container mx-auto mt-5">
       <div className="grid grid-flow-col space-x-5">
-        <div className="flex col-span-4 text-white bg-black">
-          
+        <div className="flex col-span-4 border-2 rounded-md border-rose-500">
+          <h1></h1>
         </div>
-        <div className="col-span-2 text-white bg-black">
-          Information
+        <div className="col-span-2 border-2 rounded-md border-rose-500">
+          <h1 className="text-2xl text-center font-bold">Information</h1>
         </div>
       </div>
     </div>
