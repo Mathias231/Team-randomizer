27bb5d8adf2aaeb95f667bb032ad47eb27ca10ce
Mathias231 <mathias.krogh@outlook.com>
2022-06-17 15:15:36 +0200

    new start

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 // Importing components
-import Input from './components/Input/Input';
+// import Input from './components/Input/Input';
 
 function App() {
   return (
-    <div className="App">
-      <Input />
+    <div className="container mx-auto mt-5">
+      <div className="grid grid-flow-col space-x-5">
+        <div className="flex col-span-4 text-white bg-black">
+          
+        </div>
+        <div className="col-span-2 text-white bg-black">
+          Information
+        </div>
+      </div>
     </div>
   );
 }
