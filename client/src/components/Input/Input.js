d3237da9cc1d09677a2582045a22aa93c7f8fd92
Mathias231 <mathias.krogh@outlook.com>
2022-06-17 16:31:36 +0200

    fixing textarea

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -4,10 +4,8 @@ export default function Input() {
     
     return(
       <textarea
-      id="about"
-      name="about"
-      rows={5}
-      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+      rows={6}
+      className="shadow-sm w-96 border block border-red-300 rounded-md overflow-auto"
       placeholder="Team name"
       defaultValue={''}
     />
