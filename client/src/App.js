3c9ee496728c5cc1d9336d30eee242b0b509abe2
Mathias231 <mathias.krogh@outlook.com>
2022-06-21 15:14:42 +0200

    Bigger text size

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,11 @@ function App() {
     <div className="container mx-auto mt-5">
       <div className="grid grid-flow-col space-x-5">
         <div className="col-span-4 border-2 rounded-md border-rose-500">
-          <h1 className="text-2xl text-center font-bold">Generate Team</h1>
+          <h1 className="text-3xl text-center font-bold">Generate Team</h1>
           <Randomize />
         </div>
         <div className="col-span-2 border-2 rounded-md border-rose-500">
-          <h1 className="text-2xl text-center font-bold">Information</h1>
+          <h1 className="text-3xl text-center font-bold">Information</h1>
         </div>
       </div>
     </div>
