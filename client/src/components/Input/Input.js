eab0709bbd763e1351ba0676d227191a153d323f
Mathias231 <mathias.krogh@outlook.com>
2022-06-21 15:17:23 +0200

    Removed rounded style

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -5,7 +5,7 @@ export default function Input() {
     return(
       <textarea
       rows={6}
-      className="shadow-sm w-96 border block border-red-300 rounded-md overflow-auto"
+      className="shadow-sm w-96 border block border-red-300 overflow-auto"
       placeholder="Names"
       defaultValue={''}
     />
