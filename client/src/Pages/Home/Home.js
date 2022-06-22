03d70a76a2759d7019106245fdba833d0648ce95
Mathias231 <mathias.krogh@outlook.com>
2022-06-22 09:45:56 +0200

    First commit

---
+++ client/src/Pages/Home/Home.js
@@ -0,0 +1,17 @@
+function HomePage() {
+    return (
+    <div className="container mx-auto px-0 xl:px-5 mt-5">
+        <div className="grid grid-flow-col space-x-5">
+            <div className="col-span-4 border-2 rounded-md border-rose-500">
+                <h1 className="text-3xl text-center font-bold">Generate Team</h1>
+                    
+            </div>
+            <div className="col-span-2 border-2 rounded-md border-rose-500">
+                <h1 className="text-3xl text-center font-bold">Information</h1>
+            </div>
+        </div>
+    </div>
+    );
+  }
+  
+  export default HomePage;
