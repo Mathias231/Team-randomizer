786f5f29cd774adb3d63e94c8a03849c02d91409
Mathias231 <mathias.krogh@outlook.com>
2022-06-22 09:45:51 +0200

    tidying

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,9 @@
-// Importing components
-import Randomize from './Components/Form/RandomizeForm';
-import InfoBox from './Components/InfoBox/InfoBox';
+import HomePage from './Pages/Home/Home';
 
 function App() {
   return (
-    <div className="container mx-auto px-0 xl:px-5 mt-5">
-      <div className="grid grid-flow-col space-x-5">
-        <div className="col-span-4 border-2 rounded-md border-rose-500">
-          <h1 className="text-3xl text-center font-bold">Generate Team</h1>
-          <InfoBox />
-          <Randomize />
-        </div>
-        <div className="col-span-2 border-2 rounded-md border-rose-500">
-          <h1 className="text-3xl text-center font-bold">Information</h1>
-        </div>
-      </div>
+    <div>
+      <HomePage />
     </div>
   );
 }
