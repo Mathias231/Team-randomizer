b53f1efee914abae7847b5670b191c2a4a705ba7
Mathias231 <mathias.krogh@outlook.com>
2022-06-22 09:39:28 +0200

    Removing

--- client/src/components/InfoBox/InfoBox.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function InfoBox() {
-    return (
-        <div className="col-span-7 mt-5 m-5">
-            <p>To randomize teams, write in the textbox under and select the number of teams you want</p>
-        </div>
-    )
-}
