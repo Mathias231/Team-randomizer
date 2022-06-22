283c19c37a3ac4c4e40371fd9630451be4ce9e34
Mathias231 <mathias.krogh@outlook.com>
2022-06-22 08:52:52 +0200

    Added styling for container

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import InfoBox from './Components/InfoBox/InfoBox';
 
 function App() {
   return (
-    <div className="container mx-auto mt-5">
+    <div className="container mx-auto px-0 xl:px-5 mt-5">
       <div className="grid grid-flow-col space-x-5">
         <div className="col-span-4 border-2 rounded-md border-rose-500">
           <h1 className="text-3xl text-center font-bold">Generate Team</h1>
