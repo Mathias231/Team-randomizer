9ddf2f18a6ef05bc119801a3abf53f7fe1482253
Mathias231 <mathias.krogh@outlook.com>
2022-06-22 13:59:06 +0200

    Updated text

--- client/src/Components/InfoBox/InfoBox.js
+++ client/src/Components/InfoBox/InfoBox.js
@@ -1,10 +1,10 @@
 export default function InfoBox() {
     return(
-        <div className="mt-5 border w-96 bg-gray-50">
-            <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-optio, eaque rerum!</h1>
+        <div className="container flex">
+            <div className="m-5 w-full grow-0 max-w- border">
+                <h1 className="text-xl font-bold">About this project</h1>
+                <p>This is a test</p>
+            </div>
         </div>
     )
 }
