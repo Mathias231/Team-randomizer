7fe1da8cb5d3bdb36c2e54362eed8bc983a527aa
Mathias231 <mathias.krogh@outlook.com>
2022-06-22 08:53:06 +0200

    Removed text-white for InfoBox

--- client/src/components/InfoBox/InfoBox.js
+++ client/src/components/InfoBox/InfoBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function InfoBox() {
     return (
-        <div className="col-span-7 mt-5 text-white m-5">
+        <div className="col-span-7 mt-5 m-5">
             <p>To randomize teams, write in the textbox under and select the number of teams you want</p>
         </div>
     )
--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -5,7 +5,7 @@ export default function Input() {
     return(
       <textarea
       rows={6}
-      className="shadow-sm w-96 border block border-red-300 overflow-auto"
+      className="shadow-sm w-96 resize border block border-red-300 overflow-auto"
       placeholder="Names"
       defaultValue={''}
     />
