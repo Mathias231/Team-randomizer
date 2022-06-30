05d36e43955e6a151af856ce3d00aaf8792080a2
Mathias231 <mathias.krogh@outlook.com>
2022-06-30 17:05:41 +0200

    Commenting

--- client/src/Components/Form/Form.js
+++ client/src/Components/Form/Form.js
@@ -10,12 +10,15 @@ export default function Form() {
     }
 
     return(
+        // Textarea
         <div className="grid grid-cols-3 mt-5 gap-3">
             <div className="col-span-2 w-full flex">
                 <textarea className="m-5 w-full h-36 bg-gray-50 rounded-lg border border-gray-300"
                 placeholder="Names">
                 </textarea>
             </div>
+
+        {/* Select Options */}
             <div className="col-span-1 mr-5">
                 <select className="w-full px-3 py-5 my-8 rounded-lg border border-gray-300 bg-gray-50" onChange={handleChange}>
                     <option value="1">1</option>
