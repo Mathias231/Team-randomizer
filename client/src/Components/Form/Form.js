39159c1794298ec2001358d049a034997222ce2e
Mathias231 <mathias.krogh@outlook.com>
2022-07-12 22:01:49 +0200

    It workes, but not happy with it yet

--- client/src/Components/Form/Form.js
+++ client/src/Components/Form/Form.js
@@ -2,16 +2,26 @@ import React, { useState } from 'react';
 
 export default function Form() {
     // Variable for selected teams
-    const [select, setSelect] = useState("");
+    const [select, setSelect] = useState("1>");
 
-    const handleClick = (e) => {
-        // Hent value fra Select og innhold i text field
+    // Variable for text field
+    const [names, setNames] =  useState("");
+    
+    
+    const handleTextField = (e) => {
+        const value = e.target.value
+        setNames(value)
     }
 
-    const handleChange = (e) => {
+    const handleSelect = (e) => {
         const value = e.target.value
         setSelect(value)
-        console.log(value)
+    }
+
+    const handleButton = (e) => {
+        // Hent value fra Select og innhold i text field
+        console.log("her er select " + select)
+        console.log("Her er text " + names)
     }
 
     return(
@@ -19,13 +29,15 @@ export default function Form() {
         <div className="grid grid-cols-3 mt-5 gap-3">
             <div className="col-span-2 w-full flex">
                 <textarea className="m-5 w-full h-36 bg-gray-50 rounded-lg border border-gray-300"
-                placeholder="Names">
+                placeholder="Names"
+                onChange={handleTextField}>
                 </textarea>
             </div>
 
         {/* Select Options */}
             <div className="col-span-1 mr-5">
-                <select className="w-full px-3 py-5 my-8 rounded-lg border border-gray-300 bg-gray-50" onChange={handleChange}>
+                <select className="w-full px-3 py-5 my-8 rounded-lg border border-gray-300 bg-gray-50" 
+                onChange={handleSelect}>
                     <option value="1">1</option>
                     <option value="2">2</option>
                     <option value="3">3</option>
@@ -35,7 +47,8 @@ export default function Form() {
 
         {/* Button */}
             <div className="ml-5">
-                <button className="border-4 border-gray-600" onClick={handleClick}>Trykk her</button>
+                <button className="border-4 border-gray-600" 
+                onClick={handleButton}>Trykk her</button>
             </div>
         </div>
     )
